e59075ffacd5b749c7ec6cea6833c9ab30890f8e
cat <connort928@gmail.com>
2023-03-25 23:13:11 -0400

    YAMLFile

---
+++ src/main/java/me/zlaio/itemstacked/YAMLFile.java
@@ -0,0 +1,56 @@
+package me.zlaio.itemstacked;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.logging.Level;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class YAMLFile {
+    
+    private final File configFile;
+    private final FileConfiguration config;
+    private final JavaPlugin plugin;
+
+    public YAMLFile(String fileName, JavaPlugin plugin) {
+        this.plugin = plugin;
+
+        configFile = new File(plugin.getDataFolder() + fileName + ".yml");
+
+        if (!configFile.exists()) {
+            configFile.getParentFile().mkdirs();
+
+            try {
+                configFile.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            config = new YamlConfiguration();
+
+        } else config = YamlConfiguration.loadConfiguration(configFile);
+        
+        saveFile();
+    }
+
+    public String getName() {
+        return configFile.getName();
+    }
+
+    public FileConfiguration getConfig() {
+        return config;
+    }
+    
+    private void saveFile() {
+        try {
+            config.save(configFile);
+        } catch (IOException e) {
+            plugin.getLogger().log(Level.WARNING, "Couldn't save file (" + configFile.getName() + ")");
+        }
+    }
+
+
+
+}
