0ec660e266250d90a08824670effea3e6057dfac
cat <connort928@gmail.com>
2023-03-25 23:04:05 -0400

    Hello :)

---
+++ src/main/java/me/zlaio/itemstacked/ItemStacked.java
@@ -0,0 +1,13 @@
+package me.zlaio.itemstacked;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ItemStacked extends JavaPlugin {
+
+
+    @Override
+    public void onEnable() {
+    
+    }
+    
+}
